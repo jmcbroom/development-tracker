633e4588d641b46b6d3253dcfe4e12879cdec35f
Jimmy McBroom <james.mcbroom@gmail.com>
2022-02-11 20:29:54 -0500

    add 404

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+// pages/404.js
+export default function Custom404() {
+  return (
+    <div className="max-w-xl mx-auto">
+      <h2>Sorry, we couldn't find that page.</h2>
+      
+    </div>    
+  )
+
+}
