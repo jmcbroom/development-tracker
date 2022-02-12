5466ea4e342c07c19585bc8d7daba74bf2fc6ff6
Jimmy McBroom <james.mcbroom@gmail.com>
2022-02-12 00:52:50 -0500

    nicer 404

--- pages/404.js
+++ pages/404.js
@@ -1,9 +1,16 @@
 // pages/404.js
+import Link from 'next/link'
+
 export default function Custom404() {
   return (
     <div className="max-w-xl mx-auto">
-      <h2>Sorry, we couldn't find that page.</h2>
-      
+      <h2>Sorry, we couldn't find that page!</h2>
+      <p className="text-lg my-8">Instead, maybe you can..</p>
+      <ul className="list-disc list-inside text-lg">
+        <li>Go to the <Link href="/map">map</Link></li>
+        <li>Go to the <Link href="/projects">projects page</Link></li>
+        <li><Link href="/submit-a-top">Tell us</Link> about a project we don't know about!</li>
+      </ul>
     </div>    
   )
 
